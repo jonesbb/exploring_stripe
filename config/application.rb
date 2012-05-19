d8fef2599ab9f4a1e4ab5443672d12b93b403007
jonesbb <jonesbb@gmail.com>
2012-05-19 16:18:44 -0700

    update

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -55,5 +57,9 @@ module ExploringStripe
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # add /configto autoload path
+    config.autoload_paths = %W(#{config.root}/config)
+
   end
 end
