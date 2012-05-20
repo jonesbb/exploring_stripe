304ff008aae64c4db53e18a5631990e452665f99
jonesbb <jonesbb@gmail.com>
2012-05-20 15:14:51 -0700

    removed stripe keys accidently pushed

--- config/initializers/Stripe.rb
+++
@@ -1,5 +0,0 @@
-# Stripe secret key (test)
-Stripe.api_key = "kP1BqnS5HYeVm59FDhyO1eCDBjFFzZWJ"
-
-# Store public key (test) as constant b/c not needed for ruby gem, just view and js
-STRIPE_PUBLIC_KEY = "pk_86RCuw4ZTKZkj2FefRvvKUOFasBXo"
